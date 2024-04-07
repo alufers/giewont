50738f1e5726a7824d1dcf00b896ba57aeae4971
alufers <alufers@wp.pl>
2024-04-07 16:36:10 +0200

    Add basic level design for the first level

---
+++ assets/README.md
@@ -0,0 +1,4 @@
+# Art:
+
+
+https://opengameart.org/content/platformer-art-complete-pack-often-updated
---
+++ assets/tileset_base.tsx
@@ -0,0 +1,27 @@
+{ "columns":12,
+ "image":"tileset_base.png",
+ "imageheight":936,
+ "imagewidth":914,
+ "margin":0,
+ "name":"tileset_base",
+ "spacing":2,
+ "tilecount":156,
+ "tiledversion":"1.10.2",
+ "tileheight":70,
+ "tiles":[
+        {
+         "id":19,
+         "type":"ladder"
+        }, 
+        {
+         "id":21,
+         "type":"ladder"
+        }, 
+        {
+         "id":31,
+         "type":"ladder"
+        }],
+ "tilewidth":70,
+ "type":"tileset",
+ "version":"1.10"
+}
